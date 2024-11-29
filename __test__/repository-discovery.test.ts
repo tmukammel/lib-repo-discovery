3aa0f88eea76db5083f3ec5605dc68e4c9c01f20
Twaha Mukammel <t.mukammel@gmail.com>
2024-11-29 00:10:02 +0000

    add: tests

---
+++ __test__/repository-discovery.test.ts
@@ -0,0 +1,46 @@
+import { describe, expect, test, beforeAll } from '@jest/globals';
+import { RepositoryDiscovery } from '../src/repository-discovery';
+import { IRepositoryInvoker } from '../src/types/repository-invoker.interface';
+
+class TestInvoker implements IRepositoryInvoker {
+	get<Q, M>(query: Q, isCollection?: boolean): M | M[] {
+		throw new Error('Method not implemented.');
+	}
+	validate<Q, D>(query: Q, validationLogic: (data: D) => boolean): Promise<boolean> {
+		throw new Error('Method not implemented.');
+	}
+	transact<Q, T, D>(method: string, query: Q, transaction: T, data?: D): D {
+		throw new Error('Method not implemented.');
+	}
+}
+
+let repoDiscovery: RepositoryDiscovery;
+let invoker: TestInvoker;
+
+beforeAll(() => {
+	repoDiscovery = RepositoryDiscovery.instance;
+	invoker = new TestInvoker();
+});
+
+describe('lib repository discovery', () => {
+	test('creats a singleton instance is created', () => {
+		const anotherReference: RepositoryDiscovery = RepositoryDiscovery.instance;
+		expect(repoDiscovery).toBe(anotherReference);
+	});
+
+	test('can add repository invoker to the registry', () => {
+		expect(repoDiscovery.addToRepositoryInvokerRegistry('test-invoker', invoker)).toBeTruthy();
+	});
+
+	test('can not add repository invoker again to the registry', () => {
+		expect(repoDiscovery.addToRepositoryInvokerRegistry('test-invoker', invoker)).toBeFalsy();
+	});
+
+	test('can get repository invoker from registry', () => {
+		expect(repoDiscovery.getRepositoryInvoker('test-invoker')).toBeTruthy();
+	});
+
+	test('can get undefined from registry when invoker is not registered', () => {
+		expect(repoDiscovery.getRepositoryInvoker('another-invoker')).toBeUndefined();
+	});
+});
